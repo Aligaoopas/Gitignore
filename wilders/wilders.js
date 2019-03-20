d537349b6b2cf18b0aaaa6d665783d95618e09b0
Remi <aligastudioopas@gmail.com>
2019-03-20 15:45:43 +0100

    First commit

---
+++ wilders/wilders.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('I\'m Benoit Lebacq!'));
+console.log(chalk.red('I\'m Sabine Robart!'));
+console.log(chalk.green('I\'m Loukiana Vivenot!'));
+console.log(chalk.magentaBright('I\'m Jordan Bonjean!'));
